cb849dbd23d105097e834e7dca7710123b96f318
echimma <chi.ma@ericsson.com>
2014-03-11 17:38:31 +0000

    Add BST DS.

--- src/main/java/com/chima/alg/tree/bst/BinarySearchTree.java
+++ src/main/java/com/chima/alg/tree/bst/BinarySearchTree.java
@@ -1,5 +1,126 @@
 package com.chima.alg.tree.bst;
 
-public class BinarySearchTree {
+public class BinarySearchTree<Key extends Comparable<Key>, Value> {
+	private class Node {
+		private final Key key;
+		private Value val;
+		private Node left, right;
+		private int N;
 
+		public Node(final Key key, final Value val, final int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+	}
+
+	private Node root;
+
+	public void delete(final Key key) {
+		this.root = this.delete(this.root, key);
+	}
+
+	private Node delete(Node x, final Key key) {
+		if (x == null)
+			return null;
+		final int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			x.left = this.delete(x.left, key);
+		else if (cmp > 0)
+			x.right = this.delete(x.right, key);
+		else {
+			if (x.right == null)
+				return x.left;
+			if (x.left == null)
+				return x.right;
+			final Node t = x;
+			x = this.min(t.right);
+			x.right = this.deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.N = this.size(x.left) + this.size(x.right) + 1;
+		return x;
+	}
+
+	public void deleteMin() {
+		this.root = this.deleteMin(this.root);
+	}
+
+	private Node deleteMin(final Node x) {
+		if (x.left == null)
+			return x.right;
+		x.left = this.deleteMin(x.left);
+		x.N = this.size(x.left) + this.size(x.right) + 1;
+		return x;
+	}
+
+	public Value get(final Key key) {
+		return this.get(this.root, key);
+	}
+
+	private Value get(final Node node, final Key key) {
+		if (node == null)
+			return null;
+		final int cmp = key.compareTo(node.key);
+		if (cmp < 0)
+			// key value is small than node's key, which means that exist on the
+			// left child tree
+			return this.get(node.left, key);
+		else if (cmp > 0)
+			return this.get(node.right, key);
+		else
+			return node.val;
+	}
+
+	public Key max() {
+		return this.max(this.root).key;
+	}
+
+	private Node max(final Node node) {
+		// Keep go to right until no left node
+		if (node.right == null)
+			return node;
+		return this.max(node.right);
+	}
+
+	public Key min() {
+		return this.min(this.root).key;
+	}
+
+	private Node min(final Node node) {
+		// Keep go to left until no left node
+		if (node.left == null)
+			return node;
+		return this.min(node.left);
+	}
+
+	public void put(final Key key, final Value val) {
+		this.root = this.put(this.root, key, val);
+	}
+
+	private Node put(final Node node, final Key key, final Value val) {
+		if (node == null)
+			return new Node(key, val, 1);
+		final int cmp = key.compareTo(node.key);
+		if (cmp < 0)
+			// put the node to the left side
+			node.left = this.put(node.left, key, val);
+		else if (cmp > 0)
+			node.right = this.put(node.right, key, val);
+		else
+			node.val = val;
+		node.N = this.size(node.left) + this.size(node.right);
+		return node;
+	}
+
+	public int size() {
+		return this.size(this.root);
+	}
+
+	public int size(final Node node) {
+		if (node == null)
+			return 0;
+		else
+			return node.N;
+	}
 }
---
+++ src/test/java/com/chima/alg/tree/bst/BinarySearchTreeTest.java
@@ -0,0 +1,18 @@
+package com.chima.alg.tree.bst;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class BinarySearchTreeTest {
+
+	private final BinarySearchTree<String, Integer> testObj = new BinarySearchTree<String, Integer>();
+
+	@Test
+	public void test_create_first_node_as_root() {
+		final String key = "1";
+		final Integer val = 1;
+		this.testObj.put(key, val);
+		Assert.assertEquals(val, this.testObj.get(key));
+	}
+}
